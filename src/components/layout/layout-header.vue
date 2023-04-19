1504e9e1b41f35900338ea528b28fe407cefa197
Yyyy <1269825764@qq.com>
2023-04-20 00:17:51 +0800

    首页背景图的更新

--- src/components/layout/layout-header.vue
+++ src/components/layout/layout-header.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="layout-header" :class="{'fixed':fixed,'hidden':hidden}" @click.stop="mobileShow=false">
+  <div>
+  <div id="layout-header" :class="{ 'fixed': fixed, 'hidden': hidden }" @click.stop="mobileShow = false">
     <div class="site-logo">
       <router-link to="/">
         <img src="@/assets/Main/logo.png" alt="Logo" title="首页">
-<!--        <img :src="websiteInfo.avatar" alt="Logo" title="首页">-->
+        <!--        <img :src="websiteInfo.avatar" alt="Logo" title="首页">-->
       </router-link>
     </div>
-    <div class="menus-btn" @click.stop="mobileShow=!mobileShow">
+    <div class="menus-btn" @click.stop="mobileShow = !mobileShow">
       Menus
     </div>
-    <div class="site-menus" :class="{'mobileShow':mobileShow}" @click.stop="mobileShow=!mobileShow">
+    <div class="site-menus" :class="{ 'mobileShow': mobileShow }" @click.stop="mobileShow = !mobileShow">
       <div class="menu-item header-search">
-        <header-search/>
+        <header-search />
       </div>
 
       <div class="menu-item">
@@ -30,13 +31,30 @@
         <router-link to="/blockly">编程</router-link>
       </div>
       <div class="menu-item">
-        <router-link to="/friend">友链</router-link>
+        <router-link to="/MessageBoard">友链</router-link>
       </div>
       <div class="menu-item">
         <router-link to="/about">关于</router-link>
       </div>
     </div>
   </div>
+    <div class="headImgBox">
+      <div class="scene">
+        <div><span id="luke"></span></div>
+      </div>
+      <div class="h-information" >
+        <a href="">
+          <img src="../../assets/Images/headBg.jpg">
+        </a>
+        <h2 class="h-description">
+          <a>
+            {{"ようこそSPURAのブログへ！"}}
+          </a>
+        </h2>
+      </div>
+    </div>
+  
+</div>
 </template>
 
 <script>
@@ -44,20 +62,26 @@ import HeaderSearch from '../nav/header-search.vue'
 
 export default {
   name: "layout-header",
-  components: {HeaderSearch},
+  components: { HeaderSearch },
   data() {
     return {
       lastScrollTop: 0,
       fixed: false,
       hidden: false,
       category: [],
-      websiteInfo:{},
-      mobileShow: false
+      websiteInfo: {},
+      mobileShow: false,
+      test:'6546456'
     }
   },
-  mounted() {
+  mounted() {//页面元素加载完成
     window.addEventListener('scroll', this.watchScroll)
     this.getWebSiteInfo()
+    // 首页字体
+    var timer = setTimeout(function() {
+			Typeit(this.test, "#luke"); //打字机效果
+			clearTimeout(timer);
+		}, 500);
   },
   beforeDestroy() {
     window.removeEventListener("scroll", this.watchScroll)
@@ -65,8 +89,8 @@ export default {
   methods: {
     goCategory(val) {
       localStorage.removeItem("category");
-      localStorage.setItem("category",JSON.stringify(val));
-      this.$router.push({name: 'category', params: {cateId: val.id, cate: val.sort_name}});
+      localStorage.setItem("category", JSON.stringify(val));
+      this.$router.push({ name: 'category', params: { cateId: val.id, cate: val.sort_name } });
     },
     watchScroll() {
       let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
@@ -84,11 +108,11 @@ export default {
     getWebSiteInfo() {
       this.$store.dispatch('getSiteInfo').then(data => {
         this.websiteInfo = data
-      }).catch(err=>{
+      }).catch(err => {
         console.log(err)
       })
     },
-    
+
   }
 }
 </script>
@@ -102,6 +126,7 @@ export default {
   height: 80px;
   padding: 0 80px;
   display: flex;
+  background-color: #ffffff;
   justify-content: space-between;
   align-items: center;
   transition: .3s all ease;
@@ -122,6 +147,7 @@ export default {
 
 .site-logo {
   text-align: center;
+
   // overflow: hidden;
   img {
     width: 200px;
@@ -159,6 +185,7 @@ export default {
       font-weight: 500;
       font-size: 16px;
       text-decoration: none;
+
       &:hover {
         color: #ff6d6d;
       }
@@ -192,7 +219,8 @@ export default {
     -o-transition: .6s all ease;
     -ms-transition: .6s all ease;
 
-    &:before, &:after {
+    &:before,
+    &:after {
       content: '';
       position: absolute;
       width: 0;
@@ -280,7 +308,8 @@ export default {
       border: none;
       box-shadow: none;
 
-      &:before, &:after {
+      &:before,
+      &:after {
         content: '';
         position: relative;
         width: 0;
@@ -298,4 +327,151 @@ export default {
     z-index: 99;
   }
 }
-</style>
+
+
+/*头部背景图*/
+
+.headImgBox {
+  background-image: url(../../assets/Images/headBg.jpg);
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  width: 100%;
+  background-size: cover;
+  background-position: center 50%;
+  background-repeat: no-repeat;
+  // margin-top: 80px;
+  margin-bottom: 90px;
+}
+
+.h-information {
+  text-align: center;
+  width: 70%;
+  margin: auto;
+  position: relative;
+  top: 480px;
+  padding: 40px 0;
+  font-size: 16px;
+  opacity: 0.98;
+  background: rgba(230, 244, 249, 0.8);
+  border-radius: 5px;
+  z-index: 1;
+  animation: b 1s ease-out;
+  -webkit-animation: b 1s ease-out;
+}
+
+@-webkit-keyframes b {
+  0% {
+    -webkit-transform: translateY(90px);
+    transform: translateY(90px);
+  }
+
+  80% {
+    -webkit-transform: translateY(5px);
+    transform: translateY(5px)
+  }
+
+  90% {
+    -webkit-transform: translateY(-5px);
+    transform: translateY(-5px)
+  }
+
+  to {
+    -webkit-transform: translateY(0);
+    transform: translateY(0)
+  }
+}
+
+@keyframes b {
+  0% {
+    -webkit-transform: translateY(90px);
+    transform: translateY(90px);
+  }
+
+  80% {
+    -webkit-transform: translateY(5px);
+    transform: translateY(5px)
+  }
+
+  90% {
+    -webkit-transform: translateY(-5px);
+    transform: translateY(-5px)
+  }
+
+  to {
+    -webkit-transform: translateY(0);
+    transform: translateY(0)
+  }
+}
+
+.h-information img {
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+  transition: all .4s ease-in-out;
+  -webkit-transition: all .4s ease-in-out;
+  object-fit: cover;
+}
+
+.h-information img:hover {
+  transform: rotate(360deg);
+  -webkit-transform: rotate(360deg);
+}
+
+.h-information h2 {
+  margin-top: 20px;
+  font-size: 18px;
+  font-weight: 700;
+  /*font-family: 'Sigmar One';*/
+}
+
+.h-information h2 a {
+  background: linear-gradient(to right, #DF2050, #48456D);
+  -webkit-background-clip: text;
+  color: transparent;
+}
+
+.headImgBox .scene {
+  width: 100%;
+  /*height:300px;*/
+  text-align: center;
+  font-size: 100px;
+  font-weight: 200;
+  color: #fff;
+  position: absolute;
+  left: 0;
+  top: 160px;
+  font-family: 'Sigmar One', Arial;
+  text-shadow: 0 2px 2px #47456d;
+
+}
+
+.headImgBox .scene span {
+  transform: matrix(1, 0, 0, 1, 0, 0);
+  -webkit-transform: matrix(1, 0, 0, 1, 0, 0);
+  text-shadow: 1px 1px 0 #ff3f1a, -1px -1px 0 #00a7e0;
+}
+
+.saying:after {
+  content: "|";
+  font-family: Arial, sans-serif;
+  font-size: 1em;
+  /*line-height: 0;*/
+  display: inline-block;
+  vertical-align: baseline;
+  opacity: 1;
+  text-shadow: 1px 1px 0 #ff3f1a, -1px -1px 0 #00a7e0;
+  animation: caret 500ms infinite;
+}
+
+@keyframes caret {
+
+  0%,
+  100% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0;
+  }
+}</style>
